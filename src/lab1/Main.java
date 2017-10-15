ff9f12b9823dccb9edba8664fd8beaaf1c2dbb52
TangDH <answer3tl@gmail.com>
2017-10-15 20:33:15 +0800

    提交

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,5 +1,6 @@
 package lab1;
 //grlwefdew
+//212
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
--- src/lab1/MyDraw.java
+++ src/lab1/MyDraw.java
@@ -1,5 +1,6 @@
 package lab1;
 //fwqhfbweuh
+//322
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
